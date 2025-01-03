4f96c48504b8115550f08df0314494415dac75e4
mvoid <175563124+mvoidets@users.noreply.github.com>
2025-01-02 21:15:29 -0700

    Add files via upload

---
+++ client/src/app.jsx
@@ -0,0 +1,15 @@
+import './app.css';
+import { Outlet } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+}
+
+export default App;
