cacc84ef9343ff33208717098e7eb7c405d9fff9
mvoidets <mvoidets@yahoo.com>
2025-01-02 22:44:31 -0700

    changed atlas link

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://admin:yZ3hmOX3nzURVoYx@cluster0.airdz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://admin:yZ3hmOX3nzURVoYx@cluster0.airdz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/googlebooks');
 
 module.exports = mongoose.connection;
