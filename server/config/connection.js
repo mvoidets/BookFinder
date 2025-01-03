863f967469201160818377a0dce824c139c9d003
mvoidets <mvoidets@yahoo.com>
2025-01-02 21:43:50 -0700

    changed connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://dbUser:gzK8sfeDXgiqpkFc@cluster1.ptcdc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1/googlebooksDB');
 
 module.exports = mongoose.connection;
